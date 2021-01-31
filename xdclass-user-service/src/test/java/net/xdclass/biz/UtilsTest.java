2cd744927b14be59e18ded459f416cdabd457a43
贾岳霆 <8006577+jia-yueting@user.noreply.gitee.com>
2021-01-31 17:56:56 +0800

    单元测试整合

---
+++ xdclass-user-service/src/test/java/net/xdclass/biz/UtilsTest.java
@@ -0,0 +1,9 @@
+package net.xdclass.biz;
+
+import net.xdclass.UserApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = UserApplication.class)
+public class UtilsTest {
+
+}
